afa8eaf5ecff8f4a116f0a1aba76c90a049d1118
lauravg25 <lauravgarcia25@gmail.com>
2024-05-27 21:50:36 -0300

    pagina

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+
+var miBoton = document.getElementById('nav-menu');
+var navElement = document.querySelector('.nav');
+
+
+miBoton.addEventListener('click', function() {
+
+var currentDisplay = navElement.style.display;
+
+if (currentDisplay === 'none' || currentDisplay === '') {
+  navElement.style.display = 'block';
+} else {
+  navElement.style.display = 'none';
+}
+});
